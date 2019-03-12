4fc7cd32ea662eaab03ddbf59dfafb0514c08fad
Sabrina L <sabrinali0312@gmail.com>
2019-03-12 11:19:30 -0400

    add FS

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -3,19 +3,35 @@ package com.example.app;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.multidex.MultiDex;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.fullstory.*;
+import com.fullstory.FS;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends Activity {
 
     private WebView mWebView;
+    private String useruid = "767e56a2-62d1-4640-836c-88c7b7032d14 ";
+    private Map vars = new HashMap();
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MultiDex.install(this);
         setContentView(R.layout.activity_main);
+
+        FS.identify(useruid);
+        Log.e("fullstory","identify");
+
+
 
         mWebView = findViewById(R.id.activity_main_webview);
 
@@ -27,8 +43,8 @@ public class MainActivity extends Activity {
         webSettings.setJavaScriptEnabled(true);
 
         // REMOTE RESOURCE
-        // mWebView.loadUrl("http://example.com");
-        // mWebView.setWebViewClient(new MyWebViewClient());
+         mWebView.loadUrl("http://google.com");
+         mWebView.setWebViewClient(new MyWebViewClient());
 
         // LOCAL RESOURCE
         // mWebView.loadUrl("file:///android_asset/index.html");
